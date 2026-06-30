26a0fee8824413acec51520e2ecd6f85c368d22a
SantoshaC1404 <santosha1404@gmail.com>
2026-06-30 06:23:09 +0530

    Coding Questions

--- src/strings/ReverseString.java
+++ src/strings/ReverseString.java
@@ -1,4 +1,17 @@
 package strings;
 
 public class ReverseString {
+    public static void main(String[] args) {
+        reverseString("Hello World");
+    }
+
+    public static void reverseString(String input) {
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        System.out.println("Input String: " + input);
+        System.out.println("Reversed String: " + reversed);
+    }
 }
