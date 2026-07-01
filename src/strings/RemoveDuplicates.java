ad4c0b7d2c988d3a7089b51aed7b87d6911d608d
SantoshaC1404 <santosha1404@gmail.com>
2026-07-01 06:29:18 +0530

    Coding Questions

---
+++ src/strings/RemoveDuplicates.java
@@ -0,0 +1,37 @@
+package strings;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        String input = "programming";
+        removeDuplicates(input);
+    }
+
+    /**
+     * Removing duplicates means keeping only the first occurrence of each character
+     * and removing any repeated characters.
+     * Example:
+     * Input: programming
+     * Output: progamin
+     */
+    public static void removeDuplicates(String input) {
+        String result = "";
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            // check if character already exist in the string
+            boolean found = false;
+            for (int j = 0; j < result.length(); j++) {
+                if (result.charAt(j) == ch) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                result = result + ch;
+            }
+        }
+        System.out.println("Original String: " + input);
+        System.out.println("After removing duplicates: " + result);
+    }
+}
