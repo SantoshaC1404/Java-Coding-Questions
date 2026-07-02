a3e922113945d9dba70f424987e41c51eeee344b
SantoshaC1404 <santosha1404@gmail.com>
2026-07-02 06:33:54 +0530

    Coding Questions

--- src/numbers/MissingNumber.java
+++ src/numbers/MissingNumber.java
@@ -4,6 +4,9 @@ public class MissingNumber {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 5};
         findMissingNumber(arr);
+
+        System.out.println("\n========== Missing Number using XOR ==========");
+        findMissingNumberUsingXOR(arr);
     }
 
     static void findMissingNumber(int[] arr) {
@@ -16,6 +19,25 @@ public class MissingNumber {
         }
         int missingNumber = expectedSum - actualSum;
         System.out.println("Missing number: " + missingNumber);
+    }
 
+    static void findMissingNumberUsingXOR(int[] arr) {
+        int xor1 = 0;
+        int xor2 = 0;
+        int number = arr.length + 1;
+
+        // XOR of numbers from 1 to number
+        for (int i = 1; i <= number; i++) {
+            xor1 = xor1 ^ i;
+        }
+
+        // XOR or array elements
+        for (int j : arr) {
+            xor2 = xor2 ^ j;
+        }
+
+        // Missing number
+        int missingNumber = xor1 ^ xor2;
+        System.out.println("Missing number: " + missingNumber);
     }
 }
