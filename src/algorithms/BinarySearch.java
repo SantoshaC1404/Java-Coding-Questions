c735ee0c63e4eb1889b8d23b3618020220979683
SantoshaC1404 <santosha1404@gmail.com>
2026-07-11 19:50:51 +0530

    binary search algorithm

--- src/algorithms/BinarySearch.java
+++ src/algorithms/BinarySearch.java
@@ -1,10 +1,15 @@
 package algorithms;
 
+import java.util.Arrays;
+
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50, 60, 70};
         int key = 50;
         binarySearch(arr, key);
+
+        System.out.println("\n=====Binary Search of Unsorted Array=====");
+        binarySearchOfUnsortedArray(arr, key);
     }
 
     /**
@@ -19,6 +24,27 @@ public class BinarySearch {
      * If key is larger → Search the right half.
      */
     public static void binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key) {
+                System.out.println("Element found at index: " + mid);
+                return;
+            } else if (key < arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        System.out.println("Element not found");
+    }
+
+    // Binary search of unsorted array
+    static void binarySearchOfUnsortedArray(int[] arr, int key) {
+        Arrays.sort(arr);
         int low = 0;
         int high = arr.length - 1;
 
